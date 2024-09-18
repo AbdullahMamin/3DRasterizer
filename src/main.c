50dc06ffbdbe308f3949ed84e97055d9d87b0f11
Abdullah <aabdullahmamin@gmail.com>
2024-09-18 17:34:43 +0400

    changed main to make the cube rotate

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "renderer.h"
+#include "clock.h"
 
 int main(void)
 {
@@ -22,11 +23,22 @@ int main(void)
 	camera camera = Camera(Vec3(0.f, 0.f, 0.f), 300.f/400.f, 3.14159f/4.f, 0.f, 0.f, 1.f, 1e5f);
 	setCamera(camera);
 
-	mat4 transform = mat4Multiply(TranslationTransform(0.f, 0.f, 15.f), ScaleTransform(2.f, 2.f, 2.f));
 
+	quat orientation = Quat(Vec3(1.f, 0.f, 0.f), 0.f);
+	
+	initClock();
 	while (isRendererOpen())
 	{
 		processEvents();
+
+		// Update
+		f32 delta_time = getDeltaTime();
+		quat rot1 = Quat(Vec3(1.f, 0.f, 0.f), 1.f*delta_time);
+		quat rot2 = Quat(Vec3(0.f, 1.f, 0.f), 0.5f*delta_time);
+		quat rotation = quatMultiply(rot1, rot2);
+		orientation = quatNormalize(quatMultiply(orientation, rotation));
+		mat4 transform = mat4Multiply(TranslationTransform(0.f, 0.f, 15.f), mat4Multiply(RotationTransform(orientation), ScaleTransform(2.f, 2.f, 2.f)));
+		
 
 		clearBuffer(Color(0, 0, 0, 255));
 		drawFlatObj(obj, transform, Color(255, 0, 0, 255));
