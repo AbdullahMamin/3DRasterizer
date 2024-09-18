1e2f22b9e554d7d194444ea63236ca4f93078a7c
Abdullah <aabdullahmamin@gmail.com>
2024-09-18 18:18:27 +0400

    Improved camera controls, namely by allowing to move the camera with the mouse

--- src/main.c
+++ src/main.c
@@ -21,6 +21,7 @@ int main(void)
 		puts("Couldn't load object!");
 	}
 
+	f32 camera_sensetivity = 1e-1f;
 	f32 camera_speed = 10.f;
 	camera camera = Camera(Vec3(0.f, 0.f, 0.f), 300.f/400.f, 3.14159f/4.f, 0.f, 0.f, 1.f, 1e5f);
 	setCamera(camera);
@@ -28,6 +29,7 @@ int main(void)
 	quat orientation = Quat(Vec3(1.f, 0.f, 0.f), 0.f);
 
 	initInput();
+	setMouseRelative(true);
 	initClock();
 	while (isRendererOpen())
 	{
@@ -35,27 +37,46 @@ int main(void)
 
 		// Update
 		f32 delta_time = getDeltaTime();
-		quat rot1 = Quat(Vec3(1.f, 0.f, 0.f), 1.f*delta_time);
-		quat rot2 = Quat(Vec3(0.f, 1.f, 0.f), 0.5f*delta_time);
+		quat rot1 = Quat(Vec3(1.f, 0.f, 0.f), 0*1.f*delta_time);
+		quat rot2 = Quat(Vec3(0.f, 1.f, 0.f), 0*0.5f*delta_time);
 		quat rotation = quatMultiply(rot1, rot2);
 		orientation = quatNormalize(quatMultiply(orientation, rotation));
 		mat4 transform = mat4Multiply(TranslationTransform(0.f, 0.f, 15.f), mat4Multiply(RotationTransform(orientation), ScaleTransform(2.f, 2.f, 2.f)));
 
+		if (isKeyDown("escape"))
+		{
+			break;
+		}
+		vec3 velocity = Vec3(0.f, 0.f, 0.f);
 		if (isKeyDown("W"))
 		{
-			camera.position.z += camera_speed*delta_time;
+			velocity.z += 1.f;
 		}
 		if (isKeyDown("A"))
 		{
-			camera.position.x -= camera_speed*delta_time;
+			velocity.x -= 1.f;
 		}
 		if (isKeyDown("S"))
 		{
-			camera.position.z -= camera_speed*delta_time;
+			velocity.z -= 1.f;
 		}
 		if (isKeyDown("D"))
 		{
-			camera.position.x += camera_speed*delta_time;
+			velocity.x += 1.f;
+		}
+		if (velocity.x != 0.f || velocity.z != 0.f)
+		{
+			velocity = vec3Scale(camera_speed, vec3Normalize(velocity));
+			velocity = vec3RotateByQuat(velocity, Quat(Vec3(0.f, -1.f, 0.f), camera.yaw));
+		}
+		camera.position = vec3Add(camera.position, vec3Scale(delta_time, velocity));
+		vec2 camera_movement = getMouseRelative();
+		camera.yaw -= camera_sensetivity*camera_movement.x*delta_time;
+		camera.pitch -= camera_sensetivity*camera_movement.y*delta_time;
+		if (isKeyDown("R"))
+		{
+			camera.position = Vec3(0.f, 0.f, 0.f);
+			camera.yaw = camera.pitch = 0.f;
 		}
 		setCamera(camera);
 		
